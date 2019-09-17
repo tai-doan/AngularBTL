08e75c16987ffa47c68c989c0cc6bded2763f4d2
Jacking Tai <kenvintai1707@gmail.com>
2019-09-17 22:13:21 +0700

    create component & custom layout
    custom signin page, signup page, contact page and part of homepage

--- webtracnghiem/src/app/app.module.ts
+++ webtracnghiem/src/app/app.module.ts
@@ -1,16 +1,55 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { environment } from '../environments/environment';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SigninComponent } from './signin/signin.component';
+import { SignupComponent } from './signup/signup.component';
+import { ResetpasswordComponent } from './resetpassword/resetpassword.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { FeedbackComponent } from './feedback/feedback.component';
+import { QuizComponent } from './quiz/quiz.component';
+import { ResultComponent } from './result/result.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SigninComponent,
+    SignupComponent,
+    ResetpasswordComponent,
+    AboutComponent,
+    ContactComponent,
+    FeedbackComponent,
+    QuizComponent,
+    ResultComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: '', 	component: HomeComponent},
+      { path: 'signin', 	component: SigninComponent},
+      { path: 'signup', 	component: SignupComponent},
+      { path: 'resetpassword', 	component: ResetpasswordComponent},
+      { path: 'about', 	component: AboutComponent},
+      { path: 'contact', 	component: ContactComponent},
+      { path: 'feedback', 	component: FeedbackComponent},
+      { path: 'quiz', 	component: QuizComponent},
+      { path: 'result', 	component: ResultComponent},
+      { path: 'home', 	component: HomeComponent},
+      { path: '**', 	redirectTo: 'home', pathMatch: 'full' }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
--- webtracnghiem/src/environments/environment.ts
+++ webtracnghiem/src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAkOK1cEEtnAJMlUdTMdiDp0fidJnjhbkA",
+    authDomain: "tracnghiemangular.firebaseapp.com",
+    databaseURL: "https://tracnghiemangular.firebaseio.com",
+    projectId: "tracnghiemangular",
+    storageBucket: "tracnghiemangular.appspot.com",
+    messagingSenderId: "545499328070",
+    appId: "1:545499328070:web:63060f3b4a2fe539e8e447"
+  }
 };
 
 /*
